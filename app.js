a27c0c435484a84d8b05d6b29c95be3131fe0708
amunipelumi <amuni.engr@gmail.com>
2025-03-21 19:40:00 +0100

    Application

---
+++ app.js
@@ -0,0 +1,33 @@
+require("dotenv").config()
+require("express-async-errors")
+const express = require("express")
+const connectDB = require("./databases/mongo")
+const authRouter = require("./routers/authRoute")
+const jobsRouter = require("./routers/jobsRoute")
+const { notFound, errorHandler } = require("./middlewares/error")
+
+const app = express()
+
+// Port
+const port = process.env.PORT || 8005
+
+// Public Middleware  Route
+// app.use(express.static("./public"))
+app.use(express.json())
+app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/jobs", jobsRouter)
+app.use(notFound)
+app.use(errorHandler)
+
+// Start function
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGODB_URI)
+        app.listen(port, console.log(`Listening on port ${port}...`))
+    } catch (error) {
+        console.error(error)
+        return
+    }
+}
+
+start()

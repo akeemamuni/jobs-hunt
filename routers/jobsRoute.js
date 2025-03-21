65ba17fbabb668bc6e99c537bee16ab1bf92e11e
amunipelumi <amuni.engr@gmail.com>
2025-03-21 19:38:12 +0100

    Routing

---
+++ routers/jobsRoute.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const router = express.Router()
+const authMiddleware = require("../middlewares/auth")
+const { 
+    createJob, getAllJobs, 
+    getSingleJob, updateJob, deleteJob
+} = require("../controllers/jobsController")
+
+
+router.route("/").post(createJob).get(authMiddleware, getAllJobs)
+router.route("/:id").get(authMiddleware,getSingleJob).patch(authMiddleware, updateJob).delete(authMiddleware, deleteJob)
+
+module.exports = router

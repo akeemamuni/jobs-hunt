7216a0cf1b178c324067d83d53f5b96b79a45c0a
amunipelumi <amuni.engr@gmail.com>
2025-03-22 14:53:42 +0100

    Finalize registeration controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,21 @@
+const { StatusCodes: sc } = require("http-status-codes")
+const { BadReqError } = require("../middlewares/error")
+const User = require("../models/user")
+
 const login = async (req, res) => {
     return res.status(200).json("This is the login page..")
 }
-
+// Register a new user
 const register = async (req, res) => {
-    return res.status(200).json("This is the registration page..")
+    // const { name, email, password } = req.body
+    // if (!name || !email || !password) throw new BadReqError
+    try {
+        const regUser = await User.create(req.body)
+        return res.status(sc.CREATED).json({token: regUser.createToken()})
+    } catch (error) {
+        console.log(error)
+        throw new BadReqError
+    }
 }
 
-module.exports = {
-    login, 
-    register
-}
+module.exports = { login, register }

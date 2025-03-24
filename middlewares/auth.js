a2a0a477780af9dc73059c564170d1df173afb37
amunipelumi <amuni.engr@gmail.com>
2025-03-24 17:57:54 +0100

    Updated 'authMiddleware'

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,15 @@
 const jwt = require("jsonwebtoken")
-const { UnauthError } = require("./error")
+const { InvalidAuthError } = require("./error")
 
 const authMiddleware = async (req, res, next) => {
     const { authorization: authHeader } = req.headers
     try {
-        if (!authHeader || !authHeader.startsWith("Bearer ")) throw new UnauthError
-        req.decoded = jwt.verify(authHeader.split(" ")[1], process.env.SECRET)
+        if (!authHeader || !authHeader.startsWith("Bearer ")) throw new InvalidAuthError
+        const auth = jwt.verify(authHeader.split(" ")[1], process.env.SECRET)
+        req.authUser = {userId: auth.userId, email: auth.email}
         return next()
     } catch (error) {
-        throw new UnauthError
+        throw new InvalidAuthError
     }
 }
 

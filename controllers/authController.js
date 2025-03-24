a4d7b63a67d06e4988b1900ffa4954de89d81003
amunipelumi <amuni.engr@gmail.com>
2025-03-24 18:02:16 +0100

    Password verification feature

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,33 @@
 const { StatusCodes: sc } = require("http-status-codes")
-const { BadReqError } = require("../middlewares/error")
+const { BadReqError, UnauthError } = require("../middlewares/error")
 const User = require("../models/user")
 
+// Login a user
 const login = async (req, res) => {
-    return res.status(200).json("This is the login page..")
+    const { email, password } = req.body
+    if (email.length < 5 || password.length < 8) throw new BadReqError
+    try {
+        const regUser = await User.findOne({email})
+        if (!regUser) throw new UnauthError
+        const verified = await regUser.verifyPassword(password)
+        if (!verified) throw new UnauthError
+        return res.status(sc.OK).json({
+            name: regUser.name, 
+            token: regUser.createToken()
+        })
+    } catch (error) {
+        console.log(error)
+        throw new UnauthError
+    }
 }
+
 // Register a new user
 const register = async (req, res) => {
     // const { name, email, password } = req.body
     // if (!name || !email || !password) throw new BadReqError
     try {
         const regUser = await User.create(req.body)
-        return res.status(sc.CREATED).json({token: regUser.createToken()})
+        return res.status(sc.CREATED).json(`${regUser.name}'s account created..`)
     } catch (error) {
         console.log(error)
         throw new BadReqError

f67771ef4321bb13a7c3d26b883709e1e2e7e858
amunipelumi <amuni.engr@gmail.com>
2025-03-25 17:51:01 +0100

    Complete CRUD controllers

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,41 +1,51 @@
 const JobSchema = require("../models/jobs")
 const { StatusCodes: sc } = require("http-status-codes")
-const { BadReqError, InvalidAuthError } = require("../middlewares/error")
-const jobs = require("../models/jobs")
+const { BadReqError, NotFoundError } = require("../middlewares/error")
 
 const createJob = async (req, res) => {
-    try {
-        const { company, position } = req.body
-        const { userId: createdBy } = req.authUser
-        const job = await JobSchema.create({company, position, createdBy})
-        return res.status(sc.CREATED).json(job)
-    } catch (error) {
-        console.log(error)
+    const { company, position } = req.body
+    const { userId: createdBy } = req.authUser
+    if (!company || !position || company === "" || position === "") {
         throw new BadReqError
     }
+    const job = await JobSchema.create({company, position, createdBy})
+    res.status(sc.CREATED).json(job)
 }
 
 const getAllJobs = async (req, res) => {
-    try {
-        const { userId: createdBy } = req.authUser
-        const jobs = await JobSchema.find({createdBy})
-        return res.status(sc.OK).json({total: jobs.length, jobs})
-    } catch (error) {
-        console.log(error)
-        throw new InvalidAuthError
-    }
+    const { userId: createdBy } = req.authUser
+    const jobs = await JobSchema.find({createdBy}).select(
+        "company position status"
+    )
+    if (!jobs) throw new NotFoundError("No job found..")
+    res.status(sc.OK).json({total: jobs.length, jobs})
 }
 
 const getJob = async (req, res) => {
-    res.status(200).json("single job")
+    const { id: _id } = req.params
+    const { userId: createdBy } = req.authUser
+    const job = await JobSchema.findOne({_id, createdBy}).select(
+        "-createdBy -__v"
+    )
+    if (!job) throw new NotFoundError(`No job with id: ${_id}`)
+    res.status(sc.OK).json(job)
 }
 
 const updateJob = async (req, res) => {
-    res.status(200).json("update job")
+    const { params: {id: _id}, authUser: {userId: createdBy} } = req
+    const job = await JobSchema.findOneAndUpdate(
+        {_id, createdBy}, {...req.body}, 
+        {new: true, runValidators: true}
+    )
+    if (!job) throw new NotFoundError(`No job with id: ${_id}`)
+    res.status(sc.OK).json(job)
 }
 
 const deleteJob = async (req, res) => {
-    res.status(200).json("delete job")
+    const { params: {id: _id}, authUser: {userId: createdBy} } = req
+    const job = await JobSchema.findOneAndDelete({_id, createdBy})
+    if (!job) throw new NotFoundError(`No job with id: ${_id}`)
+    res.status(sc.NO_CONTENT).json(job)
 }
 
 module.exports = {

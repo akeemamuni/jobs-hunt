54a2d45c31e5115bb4146aa994f7a3325180b898
amunipelumi <amuni.engr@gmail.com>
2025-03-26 16:03:36 +0100

    Updated with security features

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// 
 require("dotenv").config()
 require("express-async-errors")
 const express = require("express")
@@ -7,16 +8,33 @@ const jobsRouter = require("./routers/jobsRoute")
 const authMiddleware = require("./middlewares/auth")
 const { notFound, errorHandler } = require("./errors/error")
 
+// App/Port
 const app = express()
-
-// Port
 const port = process.env.PORT || 8005
 
-// Public Middleware  Route
-// app.use(express.static("./public"))
+// Security/Middleware/Route
+const cors = require("cors")
+const xss = require("xss-clean")
+const helmet = require("helmet")
+const { rateLimit } = require("express-rate-limit")
+
+const expLimiter = rateLimit({
+    limit: 100,
+    windowMs: 15 * 60 * 1000,
+    standardHeaders: true,
+    legacyHeaders: false
+})
+
 app.use(express.json())
-app.use("/api/v1/auth", authRouter)
+app.use(expLimiter)
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
+app.get("/", (req, res) => res.send("Job Hunting 101"))
 app.use("/api/v1/jobs", authMiddleware, jobsRouter)
+app.use("/api/v1/auth", authRouter)
+
 app.use(notFound)
 app.use(errorHandler)
 

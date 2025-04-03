abcf7013989e93d3ec15ce3d88aeacaff58c5b1e
amunipelumi <amuni.engr@gmail.com>
2025-04-03 16:47:26 +0100

    Database testing

---
+++ __tests__/01-db.test.js
@@ -0,0 +1,28 @@
+require("dotenv").config()
+const mongoose = require("mongoose")
+const connectDB = require("../databases/mongo")
+const { MongoMemoryServer } = require("mongodb-memory-server")
+
+describe("Database connections", () => {
+    // Setup
+    let mongoServer
+    beforeAll(async () => {
+        mongoServer = await MongoMemoryServer.create()
+    })
+    afterEach(async () => {
+        await mongoose.disconnect()
+    })
+    afterAll(async () => {
+        await mongoServer.stop()
+    })
+    // Test
+    test("Should connect to database", async () => {
+        const mongoUri = mongoServer.getUri()
+        const db = await connectDB(mongoUri)
+        expect(db).toBeDefined()
+        expect(mongoose.connection.readyState).toBe(1)
+    })
+    test("Should throw error due to wrong URI", () => {
+        expect(async () => await connectDB("fakeUri")).rejects.toThrow()
+    })
+})
